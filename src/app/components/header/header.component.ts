e43d0eceeafbf9a302af385a4024db5f4a0851c6
Junio Souza <ejunioms@gmail.com>
2023-02-09 17:20:21 -0300

    Adicionando dados ao usuário

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { UserProxy } from '../../models/userProxy';
 
 @Component({
   selector: 'app-header',
@@ -7,8 +9,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  //#region constructor
 
-  ngOnInit() {}
+  constructor(private readonly alertController: AlertController) {
+
+  }
+
+  //#endregion
+
+  //#region Public Property
+
+  public handlerMessage: string = '';
+  public roleMessage: string = '';
+  public user?: UserProxy = new UserProxy('Junio Souza', 'roxo');
+
+  //#endregion
+
+  //#region Public Methods
+
+  public ngOnInit() {
+
+  }
+
+  public async onDevelopment(): Promise<void> {
+    const alert = await this.alertController.create({
+      header: 'Em desenvolvimento!',
+      buttons: [
+        {
+          text: 'OK',
+          role: 'confirm',
+          handler: () => {
+            this.handlerMessage = 'Alert confirmed';
+          },
+        },
+      ],
+    });
+
+    await alert.present();
+
+    const { role } = await alert.onDidDismiss();
+    this.roleMessage = `Dismissed with role: ${ role }`;
+  }
+
+  //#endregion
 
 }
